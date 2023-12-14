c0f85f473751ed0a1563937f0b7790c579635723
Miguel Rebollo <mrebollo@upv.es>
2023-12-14 07:58:07 +0100

    day 11 part 1 working on test, fail on complete input

--- day11/day1101.cpp
+++ day11/day1101.cpp
@@ -7,22 +7,37 @@
 #include <string>
 #include <vector>
 
-#define N 10
+#define N 140 //10
 using namespace std;
 char space[N][N];
 
-void load_file(string filename){
+struct coord{
+    int i;
+    int j;
+};
+
+
+vector<coord> load_file(string filename){
+    vector<coord> galaxies;
+    coord gcoor;
     ifstream f;
     int i = 0, j = 0;
     char c;
-    int galaxy = 0;
     f.open(filename);
     while (f >> c) {
-        space[i][(j++) % N] = c;
-        if (j % N == 0)
+        if(c == '#') {
+            gcoor.i = i;
+            gcoor.j = j;
+            galaxies.push_back(gcoor);
+        }
+        space[i][j++] = c;
+        if (j == N){
             i++;
-    }   
+            j = 0;
+        }
+    }
     f.close();
+    return galaxies;
 }
 
 
@@ -35,27 +50,62 @@ void print_map(){
 }
 
 
-char** expanse(char space[][N]){
-    return NULL;
+void expanse_rows(vector<coord> &galaxies){
+    // expanse rows
+    int i, j;
+    for (i = 0; i < N; i++){
+        for (j = 0; j < N; j++)
+            if (space[i][j] == '#')
+                break;
+        if (j == N) // no galaxies -> add 1 to galaxy rows 
+            for (int k = 0; k < galaxies.size(); k++)
+                if (galaxies[k].i > i) (galaxies[k].i)++;
+    }
 }
 
 
-vector<int> shortest_path(char **map){
+void expanse_cols(vector<coord> &galaxies){
+    // expanse cols
+    int i, j;
+    for (j = 0; j < N; j++){
+        for (i = 0; i < N; i++)
+            if (space[i][j] == '#')
+                break;
+        if (i == N) // no galaxies -> add 1 to galaxy cols 
+            for (int k = 0; k < galaxies.size(); k++)
+                if (galaxies[k].j > j) (galaxies[k].j)++;
+    }
+}
+
+
+void expanse(vector<coord> &galaxies){
+    expanse_rows(galaxies);
+    expanse_cols(galaxies);
+}
+
+
+vector<int> shortest_path(vector<coord> galaxies){
     vector<int> pathlen;
+    for(int i = 0; i < galaxies.size(); i++)
+        for(int j = i + 1; j < galaxies.size(); j++)    
+            pathlen.push_back(abs(galaxies[i].i - galaxies[j].i) + abs(galaxies[i].j - galaxies[j].j));
     return pathlen;
 }
 
 
 int main(){
-
     vector<int> pathlen;
-    load_file("input.txt");
+    vector<coord> galaxies;
+    //galaxies = load_file("input.txt");
+    galaxies = load_file("adventofcode.com_2023_day_11_input.txt");
     print_map();
-    char **expanded = expanse(space);
-    //print_map(expanded);
-    pathlen = shortest_path(expanded);
+    expanse(galaxies);
+    pathlen = shortest_path(galaxies);
     int total = 0;
-    for(int it: pathlen)
+    for(int it: pathlen){
+        cout << it << " ";
         total += it;
+    }
+    cout << endl << "Total: " << total << endl;
     return 0;
 }   

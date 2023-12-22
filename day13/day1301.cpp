10e54f33bcc012b3340cb6bb763261538a3cd058
Miguel Rebollo <mrebollo@upv.es>
2023-12-22 21:51:00 +0100

    day 13 part 1 detección correcta de la posición de los espejos (horiz y vert)

--- day13/day1301.cpp
+++ day13/day1301.cpp
@@ -5,37 +5,44 @@
 #include <vector>
 
 using namespace std;
-inline int max(int a, int b) { return (a > b)? a : b; }
+struct solution {
+    int len, idx;
+};
+
+inline solution max(solution a, solution b) { return (a.len > b.len)? a : b; }
+
 
 /* 
     encontrar el palíndromo más largo usando programación dinámica 
     siempre es de longitud par
 */
-int findvertical(string line, int i, int j){
-    int mirror_at = -1;
+solution findvertical(string line, int i, int j){
+    int len = 0;
     if ( i >= j)
-        return -INT_MAX;
-    if(j - i == 1)
-        return i;
-    if (line[i] == line[j])
-        mirror_at = findvertical(line, i+1, j-1);
-    else
-        mirror_at = max(findvertical(line, i+1, j), findvertical(line, i, j-1));
-    return mirror_at;
+        return {0, -1};
+    int begin = i, end = j;
+    while(end > begin && line[begin++] == line[end--])
+        len += 2;
+    if (begin > end)
+        return {len, end};
+    else 
+        return max(findvertical(line, i+1, j), findvertical(line, i, j-1));
+   
 }
 
-/* igual que vertical, con cadenas completas */
-int findhorizontal(vector<string> row, int i, int j){
-    int mirror_at = -1;
+
+/* igual que vertical, con cadenas completas */ 
+solution findhorizontal(vector<string> row, int i, int j){
+    int len = 0;
     if ( i >= j)
-        return -INT_MAX;
-    if(j - i == 1)
-        return i;
-    if (row[i] == row[j])
-        mirror_at = findhorizontal(row, i+1, j-1);
-    else
-        mirror_at = max(findhorizontal(row, i+1, j), findhorizontal(row, i, j-1));
-    return mirror_at;
+        return {0, -1};
+    int begin = i, end = j;
+    while(end > begin && row[begin++] == row[end--])
+        len += 2;
+    if (begin > end)
+        return {len, end};
+    else 
+        return max(findhorizontal(row, i+1, j), findhorizontal(row, i, j-1));
 }
 
 
@@ -47,21 +54,25 @@ int main() {
     //inputf.open("adventofcode.com_2023_day_13_input.txt");
     int total_pred = 0;
     vector<string> row;
+    solution reflexions;
     while(getline(inputf, line)) {
-        // busca el espejo entre filas cuando acaba el patron
+        // 2. busca el espejo entre filas cuando acaba el patron
         if(line.empty()){
-            int center = findhorizontal(row, 0, row.size()-1);
-            cout << " - hor. center: " << center << endl;
+            reflexions = findhorizontal(row, 0, row.size()-1);
+            cout << "horizontal - size: " << reflexions.len;
+            cout << " - center: " << reflexions.idx << endl;
             cout << "--" << endl;
             row.clear();
             continue;
         }
         row.push_back(line);
-        // busca el espejo entre columnas en cada fila
+        // 1. busca el espejo entre columnas en cada fila
         // existe si esta en la misma posicion en todas las filas (TODO)
-        int center = findvertical(line, 0, line.size()-1);
-        cout << line << " - vert. center: " << center << endl;
-
+        
+        reflexions = findvertical(line, 0, line.size()-1);
+        cout << line;
+        cout << " - vertical size: " << reflexions.len;
+        cout << " - center: " << reflexions.idx << endl;
         //TODO: calcular el valor de cada patrón y acumularlos
     }
     inputf.close();

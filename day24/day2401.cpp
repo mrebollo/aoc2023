1947d3f9e5c27ed161195c3b9ad654503aaf44cc
Miguel Rebollo <mrebollo@upv.es>
2023-12-24 17:45:18 +0100

    day24 part 1 work on the test, fails on input (none inside)

--- day24/day2401.cpp
+++ day24/day2401.cpp
@@ -11,39 +11,34 @@ enum coord {x, y, z};
 
 class Stone{
     private:
-        long int pos[3], vel[3];
+        double pos[3], vel[3];
         int dim;
     public:
-        Stone(long int px, long int py, long int pz, 
-              long int vx, long int vy, long int vz);
-        void move();
-        void print();
+        Stone(double px, double py, double pz, 
+              double vx, double vy, double vz);
         //pendiente = (y2-y1)/(x2-x1) = vy/vx
-        inline float m() {return (float)vel[y] / vel[x]; }
+        inline double m() {return (double)vel[y] / vel[x]; }
         // recta que pasa por (0,0) y (x,y) es (y - y0) = m(x - x0)
-        inline float n() {return pos[y] - m() * pos[x]; }
+        inline double n() {return pos[y] - m() * pos[x]; }
+        // check if point is before the stone
+        inline bool before(double point[]) {return point[x] < pos[x] && vel[x] > 0 ||  point[x] > pos[x] && vel[x] < 0; }
         friend ostream& operator<<(ostream& os, const Stone& s);
+
 };  
 
-Stone::Stone(long int px, long int py, long int pz, 
-             long int vx, long int vy, long int vz){
-    pos[x] = px; pos[y] = py; pos[z] = pz;
-    vel[x] = vx; vel[y] = vy; vel[z] = vz;
+Stone::Stone(double px, double py, double pz, 
+             double vx, double vy, double vz){
+    pos[x] = px*1e-6; pos[y] = py*1e-6; pos[z] = pz*1e-6;
+    vel[x] = vx*1e-6; vel[y] = vy*1e-6; vel[z] = vz*1e-6;
     dim = DIM;
 }
 
-void Stone::move(){
-    for(int i = 0; i < dim; i++){
-        pos[i] += vel[i];
-    }
-}
 
 ostream& operator<<(ostream& os, const Stone& s){
-            os << s.pos[x] << ", " << s.pos[y] << ", " << s.pos[z] << " @ ";
-            os << s.vel[x] << ", " << s.vel[y] << ", " << s.vel[z] ;
-            return os;
-        }
-
+    os << s.pos[x] << ", " << s.pos[y] << ", " << s.pos[z] << " @ ";
+    os << s.vel[x] << ", " << s.vel[y] << ", " << s.vel[z] ;
+    return os;
+}
 
 
 vector<Stone> loadFile(string filename){
@@ -65,22 +60,28 @@ void print(vector<Stone> stones){
     }
 }
 
-    // stone inside area
-    inline bool inside(float point[], int area[]){
-        for(int i = 0; i < DIM; i++)
-            if(point[i] < area[0] || point[i] > area[1])
-                return false;
-        return true;
-    }
+// stone inside area
+bool inside(double point[], double area[]){
+    for(int i = 0; i < DIM; i++)
+        if(point[i] < area[0] || point[i] > area[1])
+            return false;
+    return true;
+}
 
 
-bool collision(Stone a, Stone b, int area[]){
+
+
+bool collision(Stone a, Stone b, double area[]){
     bool collision = false;
-    cout << "Hailstone A: " << a << endl;
-    cout << "Hailstone B: " << b << endl;
+    //cout << "Hailstone A: " << a << endl;
+    //cout << "Hailstone B: " << b << endl;
     if(a.m() != b.m()){
-        float col[DIM];
+        double col[DIM];
         col[x] = (b.n() - a.n()) / (a.m() - b.m());
+        if(a.before(col) || b.before(col)){
+            cout << "Collision in the past (" << col[x] << ", " << col[y] << ")" << endl;
+            return false;
+        }
         col[y] = a.m() * col[x] + a.n();
         if (inside(col, area)){
             cout << " (*) Collision inside area (" << col[x] << ", " << col[y] << ")" << endl;
@@ -94,7 +95,7 @@ bool collision(Stone a, Stone b, int area[]){
     return collision;
 }
 
-int countCollisions(vector<Stone> stones, int area[]){
+int countCollisions(vector<Stone> stones, double area[]){
     int collisions = 0;
     for(int i = 0; i < stones.size(); i++){
         for(int j = i + 1; j < stones.size(); j++){
@@ -107,8 +108,10 @@ int countCollisions(vector<Stone> stones, int area[]){
 
 int main(){
     vector<Stone> stones = loadFile("input.txt");
-    print(stones);
-    int area[] = {7,27};
+    //vector<Stone> stones = loadFile("adventofcode.com_2023_day_24_input.txt");
+    //print(stones);
+    double area[] = {7*1e-6,27*1e-6};
+    //double area[2] = {200000000,400000000};
     int numcol = countCollisions(stones, area); 
     cout << "Number of collisions: " << numcol << endl;
     return 0;
